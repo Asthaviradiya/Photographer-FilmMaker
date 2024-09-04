da636c21dbfe4a48dc1cfb8b35b8ace258b8eb6d
Asthaviradiya <asthaviradiya114@gmail.com>
2024-09-04 21:47:24 +0530

    contact js file updated

--- photographer-filmmaker/src/pages/Contact.js
+++ photographer-filmmaker/src/pages/Contact.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import WomanImg from '../img/contact/woman (1).png'
 
 const Contact = () => {
   return (
-    <section className='section bg-amber-600'></section>
+    <section className='section'>
+      <div className="container mx-auto h-full">
+        <div className='flex flex-col lg:flex-row h-full items-center justify-start pt-36 gap-x-8 text-center lg:text-left '>
+          <div>bg</div>
+          <div className='lg:flex-1 lg:pt-32 px-4'>
+            <h1 className='h1'>Contact me</h1>
+            <p className='mb-12'>I would love to get suggestions from you</p>
+            <form className= ''>
+              <div>
+                <input className='outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]' type="text" placeholder='your name'/>
+                <input className='outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]' type="text" placeholder='your email address'/>
+              </div>
+            </form>
+          </div>
+          <div>img</div>
+        </div>
+      </div>
+    </section>
   )
 }
 
