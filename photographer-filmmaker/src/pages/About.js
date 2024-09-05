763cf6343e75143e32d10be6ef70b7062f4a17e7
Asthaviradiya <asthaviradiya114@gmail.com>
2024-09-05 21:12:11 +0530

    animeroute, about and home js file updated

--- photographer-filmmaker/src/pages/About.js
+++ photographer-filmmaker/src/pages/About.js
@@ -7,9 +7,9 @@ const About = () => {
   return (
     <section className='section'>
       <div className='container mx-auto h-full relative'>
-        <div className='flex flex-col lg:flex-row h-full items-center justify-center gap-x-15 text-center lg:text-left lg:pt-16'>
+        <div className='flex flex-col lg:flex-row h-full items-center justify-center gap-x-20 text-center lg:text-left lg:pt-16'>
           <div className='flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden'>
-          <img src={WomanImg} className='h-[550px]'></img>
+          <img src={WomanImg}></img>
           </div>
           <div className='flex-1 pt-36 pb-14 lg:pt-0 lg: w-auto z-10 flex flex-col justify-center items-center lg:items-start'>
             <h1 className='h1'>About me</h1>
--- photographer-filmmaker/src/transitions.js
+++ photographer-filmmaker/src/transitions.js
@@ -1,5 +1,9 @@
+import { easeInOut } from "framer-motion";
+
 export const transition1 = {
     duration: 1.4,
-    ease: [0.6, 0.01, -0.05, 0.9],
+    // ease: [0.6, 0.01, -0.05, 0.9],
+    easeInOut: 'cubic-bezier(0.6, 0.01, 0.25, 0.9)'
+    // easing: 'cubic-bezier(0.6, 0.01, -0.05, 0.9)'
     // ease: [0.43, 0.13, 0.23, 0.96],
   };
