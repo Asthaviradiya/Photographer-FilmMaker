285ac49ac957c1dce6ca0c0102ba693d809d08ca
Asthaviradiya <asthaviradiya114@gmail.com>
2024-09-09 19:30:15 +0530

    contact.js file is updated

--- photographer-filmmaker/src/pages/Contact.js
+++ photographer-filmmaker/src/pages/Contact.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import WomanImg from '../img/contact/woman (1).png'
+import {motion} from 'framer-motion'
+import { transition1 } from '../transitions'
 
 const Contact = () => {
   return (
-    <section className='section'>
+    <motion.section initial= {{opacity: 0, y:'100%'}} animate={{opacity: 1, y: 0}} exit={{opacity: 0, y:'100%'}} transition={transition1} className='section'>
       <div className="container mx-auto h-full">
         <div className='flex flex-col lg:flex-row h-full items-center justify-start pt-36 gap-x-8 text-center lg:text-left '>
-          <div className='hidden lg:flex bg-[#eef7f9] absolute bottom-0 left-0 right-0 top-72 -z-10'></div>
+          <motion.div initial= {{opacity: 0, y:'100%'}} animate={{opacity: 1, y: 0}} exit={{opacity: 0, y:'100%'}} transition={transition1} className='hidden lg:flex bg-[#eef7f9] absolute bottom-0 left-0 right-0 top-72 -z-10'></motion.div>
           <div className='lg:flex-1 lg:pt-32 px-4'>
             <h1 className='h1'>Contact me</h1>
             <p className='mb-12 sm:mb-9'>I would love to get suggestions from you</p>
@@ -19,12 +21,12 @@ const Contact = () => {
               <button className='btn mb-[30px] mx-auto lg:mx-0 self-start'>Send it</button>
             </form>
           </div>
-          <div className='lg:flex-1'>
+          <motion.div initial= {{opacity: 0, y:'100%'}} animate={{opacity: 1, y: 0}} exit={{opacity: 0, y:'100%'}} transition={{transition: transition1, duration:1.5}} className='lg:flex-1'>
             <img className='h-[600px]' src={WomanImg} alt="" />
-          </div>
+          </motion.div>
         </div>
       </div>
-    </section>
+    </motion.section>
   )
 }
 
