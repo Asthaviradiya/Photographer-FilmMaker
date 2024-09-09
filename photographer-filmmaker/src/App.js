ca6b62c63d399c4caa3658ba3d6e2d7e612eb0be
Asthaviradiya <asthaviradiya114@gmail.com>
2024-09-09 21:05:48 +0530

    app and index file is updated

--- photographer-filmmaker/src/App.js
+++ photographer-filmmaker/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Header from './components/Header'
 import AnimeRoutes from './components/AnimRoutes'
 import {BrowserRouter as Router} from 'react-router-dom'
-// import {motion} from 'frame-motion'
+import {motion} from 'framer-motion'
 function App() {
   return (
+    <>
     <Router>
       <Header/>
       <AnimeRoutes/>
     </Router>
+    </>
   );
 }
 
